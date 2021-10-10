f5bfa447cf445089cee7d78645b5158cfee6f0c7
caoyingjunz <cao.yingjunz@gmail.com>
2021-10-10 21:41:32 +0800

    Merge template with []byte

--- practise/template-practise/main.go
+++ practise/template-practise/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
 	"os"
 	"text/template"
 
 	pt "go-learning/practise/template-practise/template"
 )
 
-//  参考 https://www.cnblogs.com/f-ck-need-u/p/10053124.html
+// 参考 https://www.cnblogs.com/f-ck-need-u/p/10053124.html
+// 渲染 + 通用模板合并
+
+const (
+	// YAMLDocumentSeparator is the separator for YAML documents
+	YAMLDocumentSeparator = "---\n"
+)
 
 type Friend struct {
 	Name string `json:"name"`
@@ -18,6 +26,10 @@ type Person struct {
 	Emails   []string          `json:"emails"`
 	Friends  []Friend          `json:"friends"`
 	Mods     map[string]string `json:"mods"`
+}
+
+func BytesCombine(pBytes ...[]byte) []byte {
+	return bytes.Join(pBytes, []byte(YAMLDocumentSeparator))
 }
 
 func main() {
@@ -53,5 +65,15 @@ func main() {
 	tpl.Execute(f, p)
 
 	// 写入标准输出
-	tpl.Execute(os.Stdout, p)
+	//tpl.Execute(os.Stdout, p)
+
+	b, err := ioutil.ReadFile("./service.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	nb := BytesCombine([]byte(pt.CommonService), b)
+	if err = ioutil.WriteFile("./merge-service.yaml", nb, 0640); err != nil {
+		panic(err)
+	}
 }
