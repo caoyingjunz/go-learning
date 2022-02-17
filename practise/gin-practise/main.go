0892c7dc9e7627ed0bd3e4ec3e5d56211c123113
caoyingjunz <cao.yingjunz@gmail.com>
2022-02-17 22:47:03 +0800

    Add gin mid demo (#45)

--- practise/gin-practise/main.go
+++ practise/gin-practise/main.go
@@ -2,26 +2,32 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+
 	"go-learning/practise/gin-practise/endpoint"
 	"go-learning/practise/gin-practise/middleware"
 	"go-learning/practise/gin-practise/worker"
 )
 
 var https = `
+###
 GET http://127.0.0.1:8000/practise/get
 
+###
 POST http://127.0.0.1:8000/practise/post
 
 {
-"name": "caoyingjun",
-"obj": {"k1": "v1", "k2": {"subk2": "subv2"}, "k3": "v3"},
-"pers": {"id": 123, "age": 19, "sex": "boy", "max": {"k1": "v1"}, "lis": ["1", "2", "3"]}
+	"name": "caoyingjun",
+	"obj": {"k1": "v1", "k2": {"subk2": "subv2"}, "k3": "v3"},
+	"pers": {"id": 123, "age": 19, "sex": "boy", "max": {"k1": "v1"}, "lis": ["1", "2", "3"]}
 }
 
+###
 POST http://127.0.0.1:8000/practise/queue?queue=test
 
+###
 POST http://127.0.0.1:8000/practise/queue/after?after=after
 
+###
 GET http://127.0.0.1:8000/practise/pod?name=mariadb-0&namespace=kubez-sysns&key=config
 `
 
@@ -33,6 +39,11 @@ type options struct {
 
 func (c *options) Register() {
 	c.engine.Use(middleware.LoggerToFile(), middleware.Auth)
+
+	m := c.engine.Group("middleware", middleware.AllowAccess())
+	{
+		m.POST("", endpoint.PostMid)
+	}
 
 	p := c.engine.Group("/practise")
 	{
