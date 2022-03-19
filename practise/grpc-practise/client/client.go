6fe5863d96d93206756f8dcc88bae1db4419cf6d
caoyingjunz <cao.yingjunz@gmail.com>
2022-03-20 01:59:21 +0800

    Add grpc practise demo

--- practise/grpc-practise/client/client.go
+++ practise/grpc-practise/client/client.go
@@ -2,27 +2,34 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pd "go-learning/practise/grpc-practise/helloworld"
 )
 
 var (
-	addr = "127.0.0.1:30360"
+	addr = "127.0.0.1:30000"
 )
 
 func main() {
-	conn, err := grpc.Dial("127.0.0.1", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("connect to rpc server failed %v", err)
+		log.Fatalf("failed to connect rpc server %v", err)
 	}
 	defer conn.Close()
+
+	c := pd.NewPixiuerClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	fmt.Println(ctx)
-
+	r, err := c.SayHello(ctx, &pd.HelloRequest{Name: "caoyingjun"})
+	if err != nil {
+		log.Fatalf("failed to sayhello %v", err)
+	}
+	log.Printf("say hello %v", r.Message)
 }
--- practise/grpc-practise/server/server.go
+++ practise/grpc-practise/server/server.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+// https://grpc.io/docs/languages/go/quickstart/
 
+import (
+	"context"
+	pd "go-learning/practise/grpc-practise/helloworld"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type server struct {
+	pd.UnimplementedPixiuerServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pd.HelloRequest) (*pd.HelloResponse, error) {
+	log.Printf("Received %v", in.Name)
+	return &pd.HelloResponse{Message: "hello " + in.GetName()}, nil
+}
+
+func main() {
+	l, err := net.Listen("tcp", ":30000")
+	if err != nil {
+		log.Fatalf("failed to listen %v", err)
+	}
+
+	s := grpc.NewServer()
+	pd.RegisterPixiuerServer(s, &server{})
+
+	log.Printf("listening at %v", l.Addr())
+	if err = s.Serve(l); err != nil {
+		log.Fatalf("failed to serve %v", err)
+	}
 }
