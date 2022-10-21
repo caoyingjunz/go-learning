c4a6646e146a91bcaaaac3b6a518d08b709fc098
caoyingjunz <cao.yingjunz@gmail.com>
2022-10-21 22:50:22 +0800

    aDD

--- practise/rbac-practise/main.go
+++ practise/rbac-practise/main.go
@@ -12,50 +12,40 @@ import (
 
 func main() {
 	// 要使用自己定义的数据库rbac_db,最后的true很重要.默认为false,使用缺省的数据库名casbin,不存在则创建
-	a := gormadapter.NewAdapter("mysql", "root:123456@tcp(127.0.0.1:13306)/casbin", true)
-	e := casbin.NewEnforcer("./model.conf", a)
+	ada := gormadapter.NewAdapter("mysql", "root:password123456@tcp(pixiu01:3306)/casbin", true)
+	enforcer := casbin.NewEnforcer("./model.conf", ada)
+
 	// 从DB加载策略
-	e.LoadPolicy()
+	if err := enforcer.LoadPolicy(); err != nil {
+		panic(err)
+	}
+	r := gin.Default()
 
-	//获取router路由对象
-	r := gin.New()
+	r.POST("/api/v1", func(c *gin.Context) {
+		enforcer.AddPolicy("admin", "/api/v1/test", "GET")
+		fmt.Println("Add policy")
+	})
 
-	r.POST("/api/v1/add", func(c *gin.Context) {
-		fmt.Println("增加Policy")
-		// AddPolicy 向当前策略添加授权规则。如果规则已经存在，函数返回false，不会添加规则。否则，该函数通过添加新规则返回 true
-		if ok := e.AddPolicy("admin", "/api/v1/hello", "GET"); !ok {
-			fmt.Println("Policy已经存在")
-		} else {
-			fmt.Println("增加成功")
+	r.DELETE("/api/v1", func(c *gin.Context) {
+		enforcer.RemovePolicy("admin", "/api/v1/test", "GET")
+		fmt.Println("delete Policy")
+	})
+
+	r.GET("/api/v1", func(c *gin.Context) {
+		policies := enforcer.GetPolicy()
+		for _, p := range policies {
+			fmt.Println("policy", p)
 		}
 	})
 
-	//删除policy
-	r.DELETE("/api/v1/delete", func(c *gin.Context) {
-		fmt.Println("删除Policy")
-		// RemovePolicy 从当前策略中删除授权规则。
-		if ok := e.RemovePolicy("admin", "/api/v1/hello", "GET"); !ok {
-			fmt.Println("Policy不存在")
-		} else {
-			fmt.Println("删除成功")
-		}
-	})
-	//获取policy
-	r.GET("/api/v1/get", func(c *gin.Context) {
-		fmt.Println("查看policy")
-		// GetPolicy 获取策略中的所有授权规则。
-		list := e.GetPolicy()
-		for _, vlist := range list {
-			for _, v := range vlist {
-				fmt.Printf("value: %s, ", v)
-			}
-		}
-	})
 	//使用自定义拦截器中间件
-	r.Use(Authorization(e))
+	r.Use(Authorization(enforcer))
 	//创建请求
-	r.GET("/api/v1/hello", func(c *gin.Context) {
-		fmt.Println("Hello 接收到GET请求..")
+	r.GET("/api/v1/test", func(c *gin.Context) {
+		fmt.Println("get v1 请求通过")
+	})
+	r.POST("/api/v1/test", func(c *gin.Context) {
+		fmt.Println("post v1 请求通过")
 	})
 
 	_ = r.Run()
@@ -70,12 +60,11 @@ func Authorization(e *casbin.Enforcer) gin.HandlerFunc {
 		//获取用户的角色
 		sub := "admin"
 
-		//判断策略中是否存在
-		if ok := e.Enforce(sub, obj, act); ok {
-			fmt.Println("恭喜您,权限验证通过")
+		if e.Enforce(sub, obj, act) {
+			fmt.Println("验证通过")
 			c.Next()
 		} else {
-			fmt.Println("很遗憾,权限验证没有通过")
+			fmt.Println("无权访问")
 			c.Abort()
 			c.String(http.StatusUnauthorized, "无权访问")
 		}
