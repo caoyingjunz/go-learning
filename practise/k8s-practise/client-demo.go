374e051fa88b50603f10a8cd89e11f5ddf03f494
caoyingjunz <cao.yingjunz@gmail.com>
2022-12-30 17:29:34 +0800

    add PartialObjectMetadata demo

--- practise/k8s-practise/client-demo.go
+++ practise/k8s-practise/client-demo.go
@@ -3,9 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/metadata"
+
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -14,18 +15,15 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	clientset, err := kubernetes.NewForConfig(config)
+
+	metadataClient, err := metadata.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	obj, err := metadataClient.Resource(schema.GroupVersionResource{Group: "apps", Version: "v1", Resource: "deployments"}).Namespace("default").Get(context.TODO(), "nginx", metav1.GetOptions{})
 	if err != nil {
 		panic(err)
 	}
 
-	services, err := clientset.CoreV1().Services("default").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	for _, d := range services.Items {
-		fmt.Println(d.Name)
-	}
-
+	fmt.Println(obj)
 }
