ffeb25b45b62f5928bc57a7cfe52a290364a9716
caoyingjunz <cao.yingjunz@gmail.com>
2023-01-05 22:09:12 +0800

    Add k8s client practise

--- practise/k8s-practise/client.go
+++ practise/k8s-practise/client.go
@@ -4,22 +4,22 @@ import (
 	"context"
 	"fmt"
 
+	"go-learning/practise/k8s-practise/app"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	config, err := clientcmd.BuildConfigFromFlags("", "/Users/caoyuan/.kube/config")
+	config, err := app.BuildClientConfig("")
 	if err != nil {
 		panic(err)
 	}
-	clientset, err := kubernetes.NewForConfig(config)
+	clientSet, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
 
-	services, err := clientset.CoreV1().Services("default").List(context.TODO(), metav1.ListOptions{})
+	services, err := clientSet.CoreV1().Services("default").List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		panic(err)
 	}
